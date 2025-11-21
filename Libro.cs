94fa4d417673e747a4229046b9552e5c91df9412
lorenzot08 <lorenzo.torcolacci.stud@ispascalcomandini.it>
2025-11-21 08:30:19 +0100

    R0.1: Creata la classe libro fino al metodo ToString

--- Libro.cs
+++ Libro.cs
@@ -8,5 +8,30 @@ namespace Verifica_Git_Biblioteca
 {
     internal class Libro
     {
+        private string _autore;
+        private string _titolo;
+        private int _annoPubblicazione;
+        private string _editore;
+        private int _numPagine;
+        public string Autore { get { return _autore; } set { _autore = value; } }
+        public string Titolo { get { return _titolo; } set { _titolo = value; } }
+        public int AnnoPubblicazione { get { return _annoPubblicazione; } set { _annoPubblicazione = value; } }
+        public string Editore { get { return _editore; } set { _editore = value; } }
+        public int NumPagine { get { return _numPagine; } set { _numPagine = value; } }
+        public Libro(string autore, string titolo, int annoPubblicazione, string editore, int numPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoPubblicazione = annoPubblicazione;
+            Editore = editore;
+            NumPagine = numPagine;
+        }
+
+        public override string ToString()
+        {
+            return $"L'autore del libro è: {Autore}, il titolo è: {Titolo}, l'anno di pubblicazione è: {AnnoPubblicazione}, l'editore è: {Editore}, le pagine totali sono: {NumPagine}";
+        }
     }
 }
+
+
