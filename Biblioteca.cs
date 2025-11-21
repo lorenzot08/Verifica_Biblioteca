bf52710f3a6940f4c7c609a65a1fb2b8a8d65050
lorenzot08 <lorenzo.torcolacci.stud@ispascalcomandini.it>
2025-11-21 08:44:30 +0100

    R0.1: Creata classe Biblioteca fino al metodo per cercare un libro a partire dal titolo

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -8,5 +8,40 @@ namespace Verifica_Git_Biblioteca
 {
     internal class Biblioteca
     {
+        public string Nome {  get; set; }
+        public string Indirizzo { get; set; }
+        public double OrarioApertura { get; set; }
+        public double OrarioChiusura { get; set; }
+        public List<Libro> Libri {  get; set; }
+
+        public Biblioteca(string nome, string indirizzo, double orarioApertura, double orarioChiusura)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            Libri = new List<Libro>();
+        }
+        public void AggiuntaLibro(Libro libro) {
+            Libri.Add(libro);
+        }
+
+        public List<Libro> CercaTitolo(string titolo)
+        {
+            List<Libro>LibriTrovatiTitolo = new List<Libro>();
+            foreach (Libro libro in Libri)
+            {
+                if (libro.Titolo.Contains(titolo))
+                {
+                    LibriTrovatiTitolo.Add(libro);
+                }
+            }
+            if (LibriTrovatiTitolo.Count == 0)
+            {
+                Console.WriteLine("Non sono stati trovati libri con questo titolo");
+            }
+            return LibriTrovatiTitolo;
+        }
+
     }
 }
